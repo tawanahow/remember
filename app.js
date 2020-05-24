af9f8afbd29d6a775d07b5dfaf873d09e897a3f8
Tawana Howard <tawanahow@gmail.com>
2020-05-24 19:11:23 -0400

    remove toggle and add match condition

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const passports = document.querySelectorAll('.board-passport');
+
+let hasFlippedPassport = false;
+let firstPassport;
+let secondPassport;
+
 function flipPassport() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!hasFlippedPassport) {
+    // first click
+    hasFlippedPassport = true;
+    firstPassport = this;
+  } else {
+    hasFlippedPassport = false;
+    secondPassport = this;
+    // console.log({ firstPassport, secondPassport });
+
+    // console.log(firstPassport.dataset.city);
+    // console.log(secondPassport.dataset.city);
+
+    if (firstPassport.dataset.city === secondPassport.dataset.city) {
+      firstPassport.removeEventListener('click', flipPassport);
+      secondPassport.removeEventListener('click', flipPassport);
+    } else {
+      setTimeout(() => {
+        firstPassport.classList.remove('flip');
+        secondPassport.classList.remove('flip');
+      }, 1500);
+    }
+    console.log('it worked');
+  }
 }
 
 passports.forEach((passport) =>
